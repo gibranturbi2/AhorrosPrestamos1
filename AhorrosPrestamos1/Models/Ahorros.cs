0feb1f0012cf1e62c41e6adab541f543052a0475
gibranturbi2 <gibranturbi@outlook.com>
2021-04-14 15:52:52 -0400

    Organizacion de tabs y namespace para ahorro

--- AhorrosPrestamos1/Models/Ahorros.cs
+++ AhorrosPrestamos1/Models/Ahorros.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace AhorrosPrestamos1.Models
 {
